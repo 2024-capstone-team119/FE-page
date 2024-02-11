7a88e0a5fd9e926381682011fa7e636699d963ec
ekkk1126 <cabi0203@naver.com>
2024-02-11 20:39:08 +0900

    feat : 커뮤니티 글쓰기 페이지 수정
    - 커뮤니티 글쓰기 페이지 레이아웃 생성 (텍스트 필드, 버튼 등)
    - 올리기 버튼 클릭시 Get.to 이용해 커뮤니티 메인으로 이동 구현
    - 텍스트 필드 유효성 검사

---
+++ allcon/lib/widget/custom_elevated_btn.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomElevatedBtn extends StatelessWidget {
+  final String text;
+  final funPageRoute;
+
+  const CustomElevatedBtn({required this.text, required this.funPageRoute});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(double.infinity, 50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      onPressed: funPageRoute,
+      child: Text('$text'),
+    );
+  }
+}
