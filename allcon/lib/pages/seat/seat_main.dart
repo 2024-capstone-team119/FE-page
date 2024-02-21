3d5411f2ec4e0ed6bb6810a633b898e07d9cccbc
antisdun <112616257+antisdun@users.noreply.github.com>
2024-02-22 04:08:17 +0900

    feat: seat_write 하단 모달창 생성

--- allcon/lib/pages/seat/seat_main.dart
+++ allcon/lib/pages/seat/seat_main.dart
@@ -1,5 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:allcon/pages/seat/seat_write.dart';
+import 'package:allcon/widget/bottom_navigation_bar.dart';
 
 class SeatMain extends StatefulWidget {
   const SeatMain({super.key});
@@ -20,68 +23,102 @@ class _SeatMainState extends State<SeatMain> {
         title: const Text("000 공연장"),
         centerTitle: true,
       ),
+      bottomNavigationBar: const MyBottomNavigationBar(
+        currentIndex: 1,
+      ),
       body: Column(
         children: [
-          const SizedBox(height: 20),
+          const SizedBox(height: 10),
           Expanded(
             child: Image.network(
               'https://example.com/your_image.jpg',
               fit: BoxFit.cover,
             ),
           ),
-          const SizedBox(height: 20),
-          reviewWrite ? reviewTab(context) : const SeatWirte(),
+          const SizedBox(height: 10),
+          reviewWrite ? reviewTab(context) : const SizedBox(),
         ],
       ),
     );
   }
 
   Widget reviewTab(BuildContext context) {
-    List<String> filter = ['추천순', '최신순'];
+    bool recommend = true;
 
     return Expanded(
-      child: Container(
-        color: Colors.grey[200],
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        reviewWrite = !reviewWrite;
-                      });
-                    },
-                    child: const Text('리뷰 작성하기'),
-                  ),
-                  Row(
-                    children: [
-                      for (String item in filter)
-                        Padding(
-                          padding:
-                              const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
-                          child: Text(item),
+      child: SingleChildScrollView(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 5.0, 8.0, 8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.fromLTRB(6.0, 0.0, 6.0, 0.0),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _showModalSheet(context);
+                        });
+                      },
+                      child: const Text(
+                        '리뷰 작성하기',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepPurple,
                         ),
-                    ],
-                  ),
-                ],
-              ),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      review(context),
-                      review(context),
-                      review(context),
-                      review(context),
-                    ],
-                  ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              recommend = true;
+                            });
+                          },
+                          child: Text(
+                            '추천순',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color:
+                                  recommend ? Colors.deepPurple : Colors.black,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              recommend = false;
+                            });
+                          },
+                          child: Text(
+                            '최신순',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color:
+                                  recommend ? Colors.black : Colors.deepPurple,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-              ),
-            ],
+                Column(
+                  children: [
+                    review(context),
+                    review(context),
+                    review(context),
+                    review(context),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -115,15 +152,15 @@ class _SeatMainState extends State<SeatMain> {
             const Text(
               "29구역 7열 오른쪽에 위치한 자리입니다. 예상보다 시야 좋았고, 브루노는 엄청나게 잘 보이지는 않지만 형체는 볼 수 있는 정도(?) 전광판으로도 충분했어요~~",
             ),
-            const SizedBox(
-              height: 10.0,
-            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Row(
                   children: [
                     const Text('Helpful ?'),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
                     TextButton(
                       onPressed: () {
                         setState(() {
@@ -154,6 +191,17 @@ class _SeatMainState extends State<SeatMain> {
           ],
         ),
       ),
+    );
+  }
+
+  // 하단 모달창 함수
+  void _showModalSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return const SeatWrite();
+      },
     );
   }
 }
---
+++ allcon/lib/pages/seat/seat_review.dart
@@ -0,0 +1,14 @@
+class Review {
+  final int id;
+  final String name;
+  final String title;
+  final String text;
+  final String createTime;
+
+  Review(
+      {required this.id,
+      required this.name,
+      required this.title,
+      required this.text,
+      required this.createTime});
+}
