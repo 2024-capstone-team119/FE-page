97478f9887996f48897d84d4e6b2c3266394149f
antisdun <112616257+antisdun@users.noreply.github.com>
2024-02-23 03:38:10 +0900

    MainHome 커밋

--- allcon/lib/Pages/MainHome/Home.dart
+++ allcon/lib/Pages/MainHome/Home.dart
@@ -7,7 +7,7 @@ import 'package:allcon/Widget/copyRight_ALLCON.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
-import 'package:allcon/Pages/ConcertHall/hallsearch.dart';
+import 'package:allcon/Pages/ConcertHall/hall_search.dart';
 import 'package:allcon/Data/Concert.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
