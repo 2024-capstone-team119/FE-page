c0e88f18e75cd9a94715df6fdfecc968455a0e71
ekkk1126 <cabi0203@naver.com>
2024-02-29 00:34:06 +0900

    feat: 전체 시스템 폰트 지정

--- allcon/lib/main.dart
+++ allcon/lib/main.dart
@@ -16,8 +16,8 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
-      // theme: ThemeData(fontFamily: 'Pretendard'),
-      // themeMode: ThemeMode.system,
+      theme: ThemeData(fontFamily: 'Pretendard'),
+      themeMode: ThemeMode.system,
     );
   }
 }
