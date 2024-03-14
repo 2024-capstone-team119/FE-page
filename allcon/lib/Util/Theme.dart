c8b48780542ed5dc703a7c6ec326ff60d52101de
ekkk1126 <cabi0203@naver.com>
2024-03-14 23:59:04 +0900

    feat : ThemeColor 추가

---
+++ allcon/lib/Util/Theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// 보라색 계열
+const lightPurple = Color(0xFFCCABDB);
+const darkPurple = Color(0xFF8474A1);
+
+//청록색 계열
+const lightMint = Color(0xFF6EC6CA);
+const Mint = Color(0xFF08979D);
+const darkMint = Color(0xFF055B5C);
