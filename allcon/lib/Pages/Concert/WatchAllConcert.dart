b320b9d509698bd990249725063e4ff4677aa888
ekkk1126 <cabi0203@naver.com>
2024-03-26 02:21:50 +0900

    feat : 태그를 통해 공연중/예정-종료 필터링하여 목록 볼 수 있도록

--- allcon/lib/Pages/Concert/WatchAllConcert.dart
+++ allcon/lib/Pages/Concert/WatchAllConcert.dart
@@ -17,6 +17,9 @@ class WatchAllConcert extends StatefulWidget {
 }
 
 class _WatchAllConcertState extends State<WatchAllConcert> {
+  bool selectedOngoing = true;
+  bool selectedFinished = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,83 +35,136 @@ class _WatchAllConcertState extends State<WatchAllConcert> {
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return const Text('데이터 없음');
           } else {
+            List<Performance> filteredPerformances = [];
+            DateFormat format = DateFormat("yyyy.MM.dd");
+            if (selectedOngoing) {
+              filteredPerformances.addAll(snapshot.data!
+                  .where((performance) => format
+                      .parse(performance.endDate!)
+                      .isAfter(DateTime.now()))
+                  .toList());
+            } else if (selectedFinished) {
+              filteredPerformances.addAll(snapshot.data!
+                  .where((performance) => format
+                      .parse(performance.endDate!)
+                      .isBefore(DateTime.now()))
+                  .toList());
+            }
+
             return Column(
               children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(width: 18),
+                    ChoiceChip(
+                      label: Text('공연중/예정'),
+                      selected: selectedOngoing,
+                      avatar: Icon(CupertinoIcons.tag_circle),
+                      selectedColor: lightMint,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      onSelected: (selected) {
+                        setState(() {
+                          selectedOngoing = selected;
+
+                          selectedFinished = false;
+                        });
+                      },
+                    ),
+                    SizedBox(width: 10),
+                    ChoiceChip(
+                      label: Text('종료공연'),
+                      selected: selectedFinished,
+                      avatar: Icon(CupertinoIcons.tag_circle),
+                      selectedColor: lightMint,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      onSelected: (selected) {
+                        setState(() {
+                          selectedFinished = selected;
+
+                          selectedOngoing = false;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
                 Expanded(
                   child: ListView.builder(
-                    itemCount: snapshot.data!.length,
+                    itemCount: filteredPerformances.length,
                     itemBuilder: (context, imgIndex) {
-                      DateFormat format = DateFormat("yyyy.MM.dd");
-                      DateTime endDate =
-                          format.parse(snapshot.data![imgIndex].endDate!);
-                      final bool isEnd = endDate.isBefore(DateTime.now());
-                      Color backgroundColor =
-                          isEnd ? Colors.black.withOpacity(0.3) : Colors.white;
-
                       return Padding(
-                        padding: const EdgeInsets.fromLTRB(20.0, 5.0, 0.0, 5.0),
+                        padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
                         child: GestureDetector(
                           onTap: () {
-                            print('항목이 클릭되었습니다: ${snapshot.data}');
+                            print(
+                                '항목이 클릭되었습니다: ${filteredPerformances[imgIndex]}');
                             Get.to(() => PerformanceDetail(
-                                  performance: snapshot.data![imgIndex],
+                                  performance: filteredPerformances[imgIndex],
                                 ));
                           },
-                          child: Container(
-                            color: backgroundColor,
-                            child: Row(
-                              children: [
-                                SizedBox(
-                                  width: 80,
-                                  child: Image.network(
-                                    snapshot.data![imgIndex].poster ?? '',
-                                    fit: BoxFit.cover,
-                                  ),
+                          child: Row(
+                            children: [
+                              SizedBox(width: 20),
+                              SizedBox(
+                                width: 80,
+                                child: Image.network(
+                                  filteredPerformances[imgIndex].poster ?? '',
+                                  fit: BoxFit.cover,
                                 ),
-                                const SizedBox(width: 16),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
+                              ),
+                              const SizedBox(width: 16),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      filteredPerformances[imgIndex]
+                                          .name
+                                          .toString(),
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 5),
+                                    if (filteredPerformances[imgIndex].cast !=
+                                            null &&
+                                        filteredPerformances[imgIndex]
+                                            .cast!
+                                            .trim()
+                                            .isNotEmpty)
                                       Text(
-                                        snapshot.data![imgIndex].name
-                                            .toString(),
-                                        maxLines: 2,
+                                        filteredPerformances[imgIndex]
+                                                .cast
+                                                .toString() ??
+                                            'Unknown Performer',
+                                        maxLines: 1,
                                         overflow: TextOverflow.ellipsis,
-                                        style: const TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                        ),
+                                        style: const TextStyle(fontSize: 14),
                                       ),
-                                      const SizedBox(height: 5),
-                                      if (snapshot.data != null &&
-                                          snapshot.data![imgIndex].cast !=
-                                              null &&
-                                          snapshot.data![imgIndex].cast!
-                                              .trim()
-                                              .isNotEmpty)
-                                        Text(
-                                          snapshot.data![imgIndex].cast
-                                                  .toString() ??
-                                              'Unknown Performer',
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: const TextStyle(fontSize: 14),
-                                        ),
-                                      const SizedBox(height: 2),
+                                    const SizedBox(height: 2),
+                                    if (filteredPerformances[imgIndex]
+                                            .startDate ==
+                                        filteredPerformances[imgIndex].endDate)
                                       Text(
-                                        '${snapshot.data![imgIndex].startDate} ~ ${snapshot.data![imgIndex].endDate}'
+                                          '${filteredPerformances[imgIndex].startDate}')
+                                    else
+                                      Text(
+                                        '${filteredPerformances[imgIndex].startDate} ~ ${filteredPerformances[imgIndex].endDate}'
                                             .toString(),
                                       ),
-                                    ],
-                                  ),
+                                  ],
                                 ),
-                                SizedBox(
-                                  width: 20,
-                                )
-                              ],
-                            ),
+                              ),
+                              SizedBox(width: 20),
+                            ],
                           ),
                         ),
                       );
