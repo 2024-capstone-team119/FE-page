d7df4959a06b71c6cb7bdb0962298392477cb6fb
antisdun <112616257+antisdun@users.noreply.github.com>
2024-03-31 02:23:16 +0900

    refactor: 커뮤니티 리스트 리팩토링

--- allcon/lib/Data/Content.dart
+++ allcon/lib/Data/Content.dart
@@ -1,4 +1,12 @@
-import 'package:get/get.dart';
+class Category {
+  int tabIdx;
+  List<Content> content;
+
+  Category({
+    required this.tabIdx,
+    this.content = const [],
+  });
+}
 
 class Content {
   final int postId;
@@ -7,15 +15,25 @@ class Content {
   final DateTime? date;
   bool isLike;
   int like;
-  List<String> comment;
+  List<Comment> comment;
 
   Content({
     required this.postId,
-    this.title = '제목없음',
-    this.like = 0,
-    this.content = '내용없음',
+    required this.title,
+    required this.content,
     this.date,
-    this.comment = const [],
+    this.like = 0,
     this.isLike = false,
+    this.comment = const [],
+  });
+}
+
+class Comment {
+  int commentId;
+  String commentContent;
+
+  Comment({
+    required this.commentId,
+    required this.commentContent,
   });
 }
