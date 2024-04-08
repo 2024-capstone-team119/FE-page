a9f5eab67f9726ba4203dc52c7219f414d1236d0
antisdun <112616257+antisdun@users.noreply.github.com>
2024-04-09 02:43:35 +0900

    feat: 올림픽 체조경기장 2층 구현

--- allcon/lib/Pages/seat/SeatLayout/Seoul/OlympicChaejo.dart
+++ allcon/lib/Pages/seat/SeatLayout/Seoul/OlympicChaejo.dart
@@ -3,6 +3,7 @@ import 'package:allcon/Util/Theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
+import 'package:flutter/widgets.dart';
 
 class OlympicChaejo extends StatelessWidget {
   const OlympicChaejo({super.key});
@@ -10,74 +11,111 @@ class OlympicChaejo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
       child: Column(
         children: [
-          firstFloor(context),
-          // Ablock(context),
+          Center(
+            child: SizedBox(
+              width: 700,
+              height: 700,
+              child: Stack(
+                children: [
+                  ...secondSeat(secondSeatCenter(context)),
+                  ...firstSeat(firstSeatCenter(context)),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Widget firstFloor(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: 600,
-        height: 600,
-        child: Stack(
-          children: List.generate(28, (index) {
-            double angle = index * math.pi / 14;
-            double x = 190 + 150 * math.cos(angle);
-            double y = 300 + 150 * math.sin(angle);
-            return Stack(
-              alignment: Alignment.center,
-              children: [
-                Positioned(
-                  left: x,
-                  top: y,
-                  child: CustomPaint(
-                    size: const Size(30, 45),
-                    painter: (index == 2 ||
-                            index == 3 ||
-                            index == 11 ||
-                            index == 12 ||
-                            index == 17 ||
-                            index == 28 ||
-                            index == 25)
-                        ? TrapezoidCurvePainter(
-                            // 빈 구역
-                            color: Colors.white,
-                            topStartX: 0,
-                            topEndX: 80,
-                            topStartY: 40,
-                            topEndY: 50,
-                            bottomStartX: 0,
-                            bottomEndX: 80,
-                            bottomStartY: 10,
-                            bottomEndY: 0,
-                            curveIntensity: 0,
-                            rotationAngle: angle * 180 / math.pi,
-                          )
-                        : TrapezoidCurvePainter(
-                            color: oneFloor,
-                            topStartX: 0,
-                            topEndX: 80,
-                            topStartY: 39,
-                            topEndY: 48,
-                            bottomStartX: 0,
-                            bottomEndX: 80,
-                            bottomStartY: 11,
-                            bottomEndY: 2,
-                            curveIntensity: 40,
-                            rotationAngle: angle * 180 / math.pi,
-                          ),
-                  ),
-                ),
-              ],
-            );
-          }),
+  List<Widget> secondSeat(Offset center) {
+    return List.generate(30, (index) {
+      double angle = index * 2 * math.pi / 30;
+      double x = center.dx + 250 * math.cos(angle) + 150;
+      double y = center.dy + 250 * math.sin(angle);
+      return Positioned(
+        left: x,
+        top: y,
+        child: CustomPaint(
+          size: const Size(30, 45),
+          painter: TrapezoidCurvePainter(
+            color: secondFloor,
+            topStartX: 0,
+            topEndX: 60,
+            topStartY: 49,
+            topEndY: 55,
+            bottomStartX: 0,
+            bottomEndX: 60,
+            bottomStartY: 3,
+            bottomEndY: -3,
+            curveIntensity: 40,
+            rotationAngle: angle * 180 / math.pi,
+          ),
         ),
-      ),
-    );
+      );
+    });
+  }
+
+  List<Widget> firstSeat(Offset center) {
+    return List.generate(28, (index) {
+      double angle = index * math.pi / 14;
+      double x = center.dx + 150 * math.cos(angle) + 150;
+      double y = center.dy + 150 * math.sin(angle);
+      return Positioned(
+        left: x,
+        top: y,
+        child: CustomPaint(
+          size: const Size(30, 45),
+          painter: (index == 2 ||
+                  index == 3 ||
+                  index == 11 ||
+                  index == 12 ||
+                  index == 17 ||
+                  index == 28 ||
+                  index == 25)
+              ? TrapezoidCurvePainter(
+                  color: Colors.white,
+                  topStartX: 0,
+                  topEndX: 80,
+                  topStartY: 40,
+                  topEndY: 50,
+                  bottomStartX: 0,
+                  bottomEndX: 80,
+                  bottomStartY: 10,
+                  bottomEndY: 0,
+                  curveIntensity: 0,
+                  rotationAngle: angle * 180 / math.pi,
+                )
+              : TrapezoidCurvePainter(
+                  color: oneFloor,
+                  topStartX: 0,
+                  topEndX: 80,
+                  topStartY: 39,
+                  topEndY: 48,
+                  bottomStartX: 0,
+                  bottomEndX: 80,
+                  bottomStartY: 11,
+                  bottomEndY: 2,
+                  curveIntensity: 40,
+                  rotationAngle: angle * 180 / math.pi,
+                ),
+        ),
+      );
+    });
+  }
+
+  // secondSeat의 중심 좌표를 계산하는 함수
+  Offset secondSeatCenter(BuildContext context) {
+    return Offset(MediaQuery.of(context).size.width / 2,
+        MediaQuery.of(context).size.height / 2);
+  }
+
+  // firstSeat의 중심 좌표를 계산하는 함수
+  Offset firstSeatCenter(BuildContext context) {
+    return Offset(MediaQuery.of(context).size.width / 2,
+        MediaQuery.of(context).size.height / 2);
   }
 }
