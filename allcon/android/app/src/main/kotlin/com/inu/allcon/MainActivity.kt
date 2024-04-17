3692d7ebf9380c1699c429fdc098c7563927042a
ekkk1126 <cabi0203@naver.com>
2024-04-18 01:54:05 +0900

    chore : 네이버 로그인 api 안드로이드 kotlin 파일 수정

--- allcon/android/app/src/main/kotlin/com/inu/allcon/MainActivity.kt
+++ allcon/android/app/src/main/kotlin/com/inu/allcon/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.inu.allcon
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterFragmentActivity()
