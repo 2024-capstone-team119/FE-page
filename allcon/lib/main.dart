d2b959f7907496fae6d9f60ca90b0b9a374ced63
antisdun <112616257+antisdun@users.noreply.github.com>
2024-04-26 15:56:11 +0900

    feat: 블루스퀘어마스터카드홀 구현

--- allcon/lib/main.dart
+++ allcon/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:allcon/Pages/Login/login.dart';
 import 'package:allcon/Pages/MainHome/Home.dart';
-import 'package:allcon/Pages/Seat/SeatLayout/Seoul/Yes24LiveHall.dart';
+import 'package:allcon/Pages/Seat/SeatLayout/Seoul/BlueSquareMasterCard.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Yes24LiveHall(),
+      home: const BlueSquareMasterCard(),
       theme: ThemeData(fontFamily: 'Pretendard'),
       themeMode: ThemeMode.system,
     );
