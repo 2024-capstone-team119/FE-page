7553bbd549cdc2b70ad8bd506fbf6b3a9c042850
ekkk1126 <cabi0203@naver.com>
2024-05-05 03:57:23 +0900

    feat : 앱 종료시 자동로그인 구현

--- allcon/lib/pages/login/controller/account_controller.dart
+++ allcon/lib/pages/login/controller/account_controller.dart
@@ -1,10 +1,13 @@
 import 'package:allcon/service/account/tokenService.dart';
 import 'package:allcon/utils/jwt.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 
 class AccountController extends GetxController {
   // 사용자의 로그인 상태 관리
   final RxBool isLogin = false.obs;
+
+  final _secureStorage = const FlutterSecureStorage();
 
   // 로그인 요청을 하고 -> 성공시 jwt 토큰을 받아옴
   final tokenService _tokenService = tokenService();
@@ -14,6 +17,9 @@ class AccountController extends GetxController {
     if (token != "-1") {
       isLogin.value = true;
       jwtToken = token;
+
+      // 토큰을 secure storage에 저장
+      await _secureStorage.write(key: 'jwt_token', value: token);
     }
     return token;
   }
@@ -21,5 +27,8 @@ class AccountController extends GetxController {
   void logout() {
     isLogin.value = false;
     jwtToken = null;
+
+    // 토큰을 secure storage에서 삭제
+    _secureStorage.delete(key: 'jwt_token');
   }
 }
