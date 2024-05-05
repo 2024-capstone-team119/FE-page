9373e63da945ac79a4ce1892e7e0654dcb9ab6dd
ekkk1126 <cabi0203@naver.com>
2024-05-05 22:41:48 +0900

    feat : 로그인 api 연결위한 서비스 수정

--- allcon/lib/pages/login/controller/account_controller.dart
+++ allcon/lib/pages/login/controller/account_controller.dart
@@ -1,34 +1,17 @@
-import 'package:allcon/service/account/tokenService.dart';
-import 'package:allcon/utils/jwt.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:allcon/service/account/loginService.dart';
 import 'package:get/get.dart';
 
 class AccountController extends GetxController {
-  // 사용자의 로그인 상태 관리
   final RxBool isLogin = false.obs;
 
-  final _secureStorage = const FlutterSecureStorage();
+  Future<bool> login(String email, String password) async {
+    bool isToken = await loginService.loginUser(email, password);
 
-  // 로그인 요청을 하고 -> 성공시 jwt 토큰을 받아옴
-  final tokenService _tokenService = tokenService();
-  Future<String> login(String email, String password) async {
-    String token = await _tokenService.login(email, password);
-
-    if (token != "-1") {
-      isLogin.value = true;
-      jwtToken = token;
-
-      // 토큰을 secure storage에 저장
-      await _secureStorage.write(key: 'jwt_token', value: token);
-    }
-    return token;
+    isLogin.value = true;
+    return isToken;
   }
 
   void logout() {
     isLogin.value = false;
-    jwtToken = null;
-
-    // 토큰을 secure storage에서 삭제
-    _secureStorage.delete(key: 'jwt_token');
   }
 }
