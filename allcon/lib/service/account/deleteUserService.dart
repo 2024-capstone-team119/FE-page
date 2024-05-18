4541e85719df05bc65f93997862925b24f03e30b
ekkk1126 <cabi0203@naver.com>
2024-05-19 00:34:11 +0900

    feat : 회원 탈퇴 api 추가

---
+++ allcon/lib/service/account/deleteUserService.dart
@@ -0,0 +1,24 @@
+import 'package:allcon/service/baseUrl.dart';
+import 'package:http/http.dart' as http;
+
+class DeleteUserService {
+  // 회원 탈퇴
+  static Future<bool> deleteUser(String userId) async {
+    var url = Uri.parse("${BaseUrl.baseUrl}delete_user/$userId");
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        print("User has been deactivated successfully.");
+        return true;
+      } else {
+        print("Failed to deactivate user: ${response.statusCode}");
+        return false;
+      }
+    } catch (e) {
+      print("Error occurred while deactivating user: $e");
+      return false;
+    }
+  }
+}
