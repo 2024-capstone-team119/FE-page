d8bf6ef18f7d16295226586660e2071a18d2150b
ekkk1126 <cabi0203@naver.com>
2024-05-21 04:44:03 +0900

    feat : 크롭 크기 수정

--- allcon/lib/pages/mypage/controller/img_crop_controller.dart
+++ allcon/lib/pages/mypage/controller/img_crop_controller.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:image_cropper/image_cropper.dart';
-import 'package:image/image.dart' as img;
-import 'package:image_cropper/image_cropper.dart';
-import 'package:image/image.dart' as img;
 import 'package:path_provider/path_provider.dart';
+import 'package:image/image.dart' as img;
 
 class ImgController extends GetxController {
   static ImgController get to => Get.find();
@@ -22,32 +19,25 @@ class ImgController extends GetxController {
 
     final croppedFile = await ImageCropper().cropImage(
       sourcePath: pickedFile.path,
-      cropStyle: CropStyle.circle,
-      aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
-      uiSettings: [
-        AndroidUiSettings(
-            toolbarTitle: 'Cropper',
-            toolbarColor: Colors.deepOrange,
-            toolbarWidgetColor: Colors.white,
-            initAspectRatio: CropAspectRatioPreset.original,
-            lockAspectRatio: true),
-        IOSUiSettings(
-          title: 'Cropper',
-        ),
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
       ],
+      aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+      cropStyle: CropStyle.circle,
     );
 
     if (croppedFile == null) {
       return null;
     }
 
-    final resizedImage = img.decodeImage(await croppedFile.readAsBytes())!;
-    final resized = img.copyResize(resizedImage, width: 120, height: 120);
-    final resizedBytes = img.encodeJpg(resized);
+    // 리사이징
+    final tempDir = await getTemporaryDirectory();
+    final uniqueId = DateTime.now().millisecondsSinceEpoch;
+    final resizedFile = File('${tempDir.path}/$uniqueId.jpg');
 
-    final directory = await getTemporaryDirectory();
-    final resizedFile = File('${directory.path}/resized.jpg')
-      ..writeAsBytesSync(resizedBytes);
+    final resizedImage = img.decodeImage(await croppedFile.readAsBytes());
+    final resized = img.copyResize(resizedImage!, width: 100);
+    resizedFile.writeAsBytesSync(img.encodeJpg(resized, quality: 90));
 
     return resizedFile;
   }
