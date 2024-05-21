4cd2ef5a2854b681f2b4f00a0323297ab13b35d9
antisdun <112616257+antisdun@users.noreply.github.com>
2024-05-22 01:06:06 +0900

    fix: 사진 첨부 수정

--- allcon/lib/pages/review/ReviewWrite.dart
+++ allcon/lib/pages/review/ReviewWrite.dart
@@ -173,17 +173,12 @@ class _ReviewWriteState extends State<ReviewWrite> {
                       },
                     ),
                     const SizedBox(
-                      height: 3.0,
+                      height: 15.0,
                     ),
-                    _imageFile == null
-                        ? const Text('No image selected.')
-                        : Image.file(
-                            File(_imageFile!.path),
-                            width: 300,
-                            height: 200,
-                            fit: BoxFit.cover,
-                          ),
                     uploadPhoto(),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
@@ -249,58 +244,103 @@ class _ReviewWriteState extends State<ReviewWrite> {
   }
 
   Widget uploadPhoto() {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      child: GridView.builder(
-        padding: const EdgeInsets.all(0),
-        shrinkWrap: true,
-        itemCount: images.length, // 보여줄 item 개수. images 리스트 변수에 담겨있는 사진 수 만큼.
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 5, // 한 행에 보여줄 사진 개수
-          childAspectRatio: 1 / 1, // 사진의 가로 세로 비율
-          mainAxisSpacing: 10, // 수평 Padding
-          crossAxisSpacing: 10, // 수직 Padding
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          // 사진 삭제 버튼을 표시하기 위해 Stack을 사용함
-          return Stack(
-            alignment: Alignment.topRight,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: FileImage(
-                      File(images[index]!
-                              .path // images 리스트 변수 안에 있는 사진들을 순서대로 표시함
-                          ),
+    return _imageFile == null
+        ? const Text('이미지가 없습니다.')
+        : Container(
+            margin: const EdgeInsets.all(10),
+            child: Stack(
+              alignment: Alignment.topRight,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: FileImage(
+                        File(_imageFile!.path),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(5),
+                Container(
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(),
+                    icon:
+                        const Icon(Icons.close, color: Colors.white, size: 15),
+                    onPressed: () {
+                      setState(() {
+                        _imageFile = null; // 이미지 삭제
+                      });
+                    },
+                  ),
                 ),
-                child: IconButton(
-                  padding: EdgeInsets.zero,
-                  constraints: const BoxConstraints(),
-                  icon: const Icon(Icons.close, color: Colors.white, size: 15),
-                  onPressed: () {
-                    setState(() {
-                      images.remove(images[index]);
-                    });
-                  },
-                ), //삭제 버튼
-              ),
-            ],
+              ],
+            ),
           );
-        },
-      ),
-    );
   }
+
+  // Widget uploadPhoto() {
+  //   return Container(
+  //     margin: const EdgeInsets.all(10),
+  //     child: GridView.builder(
+  //       padding: const EdgeInsets.all(0),
+  //       shrinkWrap: true,
+  //       itemCount: images.length, // 보여줄 item 개수. images 리스트 변수에 담겨있는 사진 수 만큼.
+  //       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+  //         crossAxisCount: 5, // 한 행에 보여줄 사진 개수
+  //         childAspectRatio: 1 / 1, // 사진의 가로 세로 비율
+  //         mainAxisSpacing: 10, // 수평 Padding
+  //         crossAxisSpacing: 10, // 수직 Padding
+  //       ),
+  //       itemBuilder: (BuildContext context, int index) {
+  //         // 사진 삭제 버튼을 표시하기 위해 Stack을 사용함
+  //         return Stack(
+  //           alignment: Alignment.topRight,
+  //           children: [
+  //             Container(
+  //               decoration: BoxDecoration(
+  //                 borderRadius: BorderRadius.circular(5),
+  //                 image: DecorationImage(
+  //                   fit: BoxFit.cover,
+  //                   image: FileImage(
+  //                     File(images[index]!
+  //                             .path // images 리스트 변수 안에 있는 사진들을 순서대로 표시함
+  //                         ),
+  //                   ),
+  //                 ),
+  //               ),
+  //             ),
+  //             Container(
+  //               decoration: BoxDecoration(
+  //                 color: Colors.black,
+  //                 borderRadius: BorderRadius.circular(5),
+  //               ),
+  //               child: IconButton(
+  //                 padding: EdgeInsets.zero,
+  //                 constraints: const BoxConstraints(),
+  //                 icon: const Icon(Icons.close, color: Colors.white, size: 15),
+  //                 onPressed: () {
+  //                   setState(() {
+  //                     images.remove(images[index]);
+  //                   });
+  //                 },
+  //               ), //삭제 버튼
+  //             ),
+  //           ],
+  //         );
+  //       },
+  //     ),
+  //   );
+  // }
 
   _showToast(String alert) {
     Widget toast = Container(
