9a48fcc6dca05b2f382a2b2cfb458d9b320ae0c6
antisdun <112616257+antisdun@users.noreply.github.com>
2024-05-30 01:20:13 +0900

    fix: likebutton 수정 중

--- allcon/lib/pages/community/controller/post_controller.dart
+++ allcon/lib/pages/community/controller/post_controller.dart
@@ -1,9 +1,11 @@
 import 'package:allcon/model/community_model.dart';
 import 'package:allcon/service/community/postService.dart';
+import 'package:allcon/service/community/likesService.dart';
 import 'package:get/get.dart';
 
 class PostController extends GetxController {
   RxList<Post> posts = <Post>[].obs;
+  RxBool isLike = false.obs;
 
   PostController._internal();
 
@@ -15,6 +17,17 @@ class PostController extends GetxController {
     try {
       final fetchedPosts = await PostService.getPost(category);
       posts.assignAll(fetchedPosts);
+    } catch (e) {
+      print('PostController 에러');
+    }
+  }
+
+  void fetchLike(String postId, String userId) async {
+    try {
+      final fetchedLike = await LikesService.isPostLiked(postId, userId);
+      print('컨트롤러: $fetchedLike');
+      isLike.value = fetchedLike;
+      print('컨트롤러 값: ${isLike.value}');
     } catch (e) {
       print('PostController 에러');
     }
