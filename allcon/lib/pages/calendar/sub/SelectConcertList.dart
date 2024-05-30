f94f20f3ec213afd51fb8cd1f61cb007dd7566fc
Shingoo Han <65566368+hansigg@users.noreply.github.com>
2024-05-30 15:12:42 +0900

    feat : 장소 및 공연 페이지 이동 추가

--- allcon/lib/pages/calendar/sub/SelectConcertList.dart
+++ allcon/lib/pages/calendar/sub/SelectConcertList.dart
@@ -1,3 +1,4 @@
+import 'package:allcon/pages/concert/PerformaceDetail.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -44,36 +45,48 @@ class _SelectConcertListState extends State<SelectConcertList> {
                       final backgroundColor =
                           index.isEven ? lavenderColor : Mint.withOpacity(0.1);
 
-                      return Container(
-                        width: MediaQuery.of(context).size.width * 0.93,
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 10.0,
-                          horizontal: 16.0,
-                        ),
-                        margin: const EdgeInsets.only(bottom: 8.0),
-                        decoration: BoxDecoration(
-                          color: backgroundColor,
-                          borderRadius: BorderRadius.circular(40.0),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                const Icon(CupertinoIcons.music_note_list),
-                                const SizedBox(width: 8),
-                                Expanded(
-                                  child: Text(
-                                    performance.name ?? '',
-                                    overflow: TextOverflow.ellipsis,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.w500,
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PerformanceDetail(
+                                performance: performance,
+                              ),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.93,
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 10.0,
+                            horizontal: 16.0,
+                          ),
+                          margin: const EdgeInsets.only(bottom: 8.0),
+                          decoration: BoxDecoration(
+                            color: backgroundColor,
+                            borderRadius: BorderRadius.circular(40.0),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  const Icon(CupertinoIcons.music_note_list),
+                                  const SizedBox(width: 8),
+                                  Expanded(
+                                    child: Text(
+                                      performance.name ?? '',
+                                      overflow: TextOverflow.ellipsis,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          ],
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       );
                     }).toList(),
@@ -103,3 +116,111 @@ class _SelectConcertListState extends State<SelectConcertList> {
     );
   }
 }
+
+
+
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:allcon/utils/Colors.dart';
+// import '../controller/selecetedDay_controller.dart';
+
+// class SelectConcertList extends StatefulWidget {
+//   const SelectConcertList({super.key});
+
+//   @override
+//   State<SelectConcertList> createState() => _SelectConcertListState();
+// }
+
+// class _SelectConcertListState extends State<SelectConcertList> {
+//   final SelectedDayController _dayController =
+//       Get.find<SelectedDayController>();
+//   bool _showMore = false;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetBuilder<SelectedDayController>(
+//       builder: (_) {
+//         final selectedDayEvents = _dayController.performances;
+
+//         if (selectedDayEvents.isEmpty) {
+//           return const SizedBox.shrink();
+//         }
+
+//         final displayedEvents =
+//             _showMore ? selectedDayEvents : selectedDayEvents.take(4).toList();
+
+//         return Theme(
+//           data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+//           child: Container(
+//             child: Column(
+//               children: [
+//                 Padding(
+//                   padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: displayedEvents.asMap().entries.map((entry) {
+//                       final index = entry.key;
+//                       final performance = entry.value;
+//                       final backgroundColor =
+//                           index.isEven ? lavenderColor : Mint.withOpacity(0.1);
+
+//                       return Container(
+//                         width: MediaQuery.of(context).size.width * 0.93,
+//                         padding: const EdgeInsets.symmetric(
+//                           vertical: 10.0,
+//                           horizontal: 16.0,
+//                         ),
+//                         margin: const EdgeInsets.only(bottom: 8.0),
+//                         decoration: BoxDecoration(
+//                           color: backgroundColor,
+//                           borderRadius: BorderRadius.circular(40.0),
+//                         ),
+//                         child: Column(
+//                           crossAxisAlignment: CrossAxisAlignment.start,
+//                           children: [
+//                             Row(
+//                               children: [
+//                                 const Icon(CupertinoIcons.music_note_list),
+//                                 const SizedBox(width: 8),
+//                                 Expanded(
+//                                   child: Text(
+//                                     performance.name ?? '',
+//                                     overflow: TextOverflow.ellipsis,
+//                                     style: const TextStyle(
+//                                       fontWeight: FontWeight.w500,
+//                                     ),
+//                                   ),
+//                                 ),
+//                               ],
+//                             ),
+//                           ],
+//                         ),
+//                       );
+//                     }).toList(),
+//                   ),
+//                 ),
+//                 if (selectedDayEvents.length > 4)
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [
+//                       IconButton(
+//                         onPressed: () {
+//                           setState(() {
+//                             _showMore = !_showMore;
+//                           });
+//                         },
+//                         icon: _showMore
+//                             ? const Icon(CupertinoIcons.chevron_compact_up)
+//                             : const Icon(CupertinoIcons.chevron_compact_down),
+//                       ),
+//                     ],
+//                   ),
+//               ],
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
--- allcon/lib/pages/concert/PerformaceDetail.dart
+++ allcon/lib/pages/concert/PerformaceDetail.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:allcon/pages/concerthall/HallMain.dart';
 import 'package:allcon/utils/Colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,6 +10,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:allcon/model/performance_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import 'package:allcon/pages/concert/PerformaceDetail.dart';
 
 class PerformanceDetail extends StatefulWidget {
   final Performance performance;
@@ -237,22 +240,51 @@ class _PerformanceDetailState extends State<PerformanceDetail> {
                   ],
                 ),
               const SizedBox(height: 3),
-              Row(
-                children: [
-                  const Icon(CupertinoIcons.placemark, size: 18),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      widget.performance.place ?? 'Unknown',
-                      style: const TextStyle(
-                        fontSize: 15.0,
-                        color: Colors.black,
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HallMain(
+                        title: widget.performance.place ?? 'Unknown',
+                        id: widget.performance.placeId ?? 'Unknown',
                       ),
-                      softWrap: true,
                     ),
-                  ),
-                ],
+                  );
+                },
+                child: Row(
+                  children: [
+                    const Icon(CupertinoIcons.placemark, size: 18),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: Text(
+                        widget.performance.place ?? 'Unknown',
+                        style: const TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.deepOrange,
+                        ),
+                        softWrap: true,
+                      ),
+                    ),
+                  ],
+                ),
               ),
+              // Row(
+              //   children: [
+              //     const Icon(CupertinoIcons.placemark, size: 18),
+              //     const SizedBox(width: 8),
+              //     Expanded(
+              //       child: Text(
+              //         widget.performance.place ?? 'Unknown',
+              //         style: const TextStyle(
+              //           fontSize: 15.0,
+              //           color: Colors.black,
+              //         ),
+              //         softWrap: true,
+              //       ),
+              //     ),
+              //   ],
+              // ),
               const SizedBox(height: 3),
               Row(
                 children: [
